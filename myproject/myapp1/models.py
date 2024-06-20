e5727b43b26fc5779eb2d0545bbbb857aaa0acb1
NikitaMartynyuk <mnekita@yandex.ru>
2024-06-20 18:47:19 +0500

    hw3

--- myproject/myapp1/models.py
+++ myproject/myapp1/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Task(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.TextField('Описание', max_length=200)
