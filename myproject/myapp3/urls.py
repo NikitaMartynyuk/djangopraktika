5ed8c54b7ee49a26d4bdc1f8b4db56d74875d63c
NikitaMartynyuk <mnekita@yandex.ru>
2024-06-25 17:28:09 +0500

    homework3

--- myproject/myapp3/urls.py
+++ myproject/myapp3/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('orders/<int:client_id>/', views.orders, name='orders'),
+    path('orders/<int:client_id>', views.orders, name='orders'),
     path('orders/', views.orders, name='all_orders'),
 ]
--- myproject/myapp3/views.py
+++ myproject/myapp3/views.py
@@ -2,7 +2,7 @@ import logging
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from datetime import timedelta
-from .models import Order, Client
+from .models import Order, Client, Product
 
 logger = logging.getLogger(__name__)
 
@@ -21,10 +21,12 @@ def orders(request, client_id: int = None):
         order = Order.objects.filter(buyer=client)
         today = timezone.now()
 
+        # Заказы клиента за период времени
         order_week = order.filter(order_date__gte=today - timedelta(days=7))
         order_month = order.filter(order_date__gte=today - timedelta(days=30))
         order_year = order.filter(order_date__gte=today - timedelta(days=365))
 
+        # Получаем уникальные продукты в заказах
         products_week = {product for order in order_week for product in order.products.all()}
         products_month = {product for order in order_month for product in order.products.all()}
         products_year = {product for order in order_year for product in order.products.all()}
@@ -40,6 +42,4 @@ def orders(request, client_id: int = None):
     else:
         order = Order.objects.all()
         context = {"title": f"Список всех заказов", "order": order}
-    return render(request, "myapp3/orders_history.html", context)
-
-
+    return render(request, "myapp3/orders.html", context)
